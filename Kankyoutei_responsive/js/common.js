f7dd17b1562631b7cad1850305cbf86bb7ed7aa1
_nondesign <yrhnico@gmail.com>
2019-11-13 12:04:40 +0900

    Kankyoutei_responsiveを追加

---
+++ Kankyoutei_responsive/js/common.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+
+  /* =====================
+  ･･･drawer Menu
+  ========================*/
+  $('.js-button-hamburger').click(function () {
+    $('body').toggleClass('is-active-drawer');
+
+    if ($(this).attr('aria-expanded') == 'false') {
+      $(this).attr('aria-expanded', true);
+    } else {
+      $(this).attr('aria-expanded', false);
+    }
+  });
+});
