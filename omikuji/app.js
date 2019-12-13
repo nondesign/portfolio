62baa88b8458d3c1583d697e682c7b0df5d04442
nondesign00 <yrhnico@gmail.com>
2019-12-13 22:26:48 +0900

    omikujiを追加

---
+++ omikuji/app.js
@@ -0,0 +1,18 @@
+﻿// resultsに結果を代入
+var results = ['大凶','凶','末吉','吉','小吉','中吉','大吉'];
+
+// btnに代入
+var btn = document.getElementById('js-button');
+
+// pを生成
+var p = document.createElement('p');
+// class名を追加
+p.className = 'text-result';
+
+
+//　クリックイベント
+btn.addEventListener('click', function() {
+  var number = Math.floor(Math.random() * results.length);
+  document.getElementById("js-result").appendChild(p);
+  p.innerHTML = results[number];
+}, false);
